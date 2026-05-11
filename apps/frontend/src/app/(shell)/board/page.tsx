27fb79e81fcb2cfbd08346ae59a87ec593109428
fellocoder <nthurakkal92@gmail.com>
2026-05-11 22:23:27 +0200

    spacing fix

--- apps/frontend/src/app/(shell)/board/page.tsx
+++ apps/frontend/src/app/(shell)/board/page.tsx
@@ -3,7 +3,7 @@ import { BoardContent } from "@/components/organisms/BoardContent";
 
 export default function BoardPage() {
   return (
-    <div className="bg-surface-bright container mx-auto flex h-full flex-col">
+    <div className="bg-surface-bright container mx-auto flex h-full flex-col md:px-8">
       <BoardViewHeader />
       <BoardContent />
     </div>
